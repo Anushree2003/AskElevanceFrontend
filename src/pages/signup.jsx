d608adebbeadced5f4076f6e8466ea14119df01a
Anushree Sinha <anushree.sinha1@ibm.com>
2026-03-04 11:04:50 +0530

    Added role in the signup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -11,7 +11,7 @@ export default function SignUp() {
     name: "",
     email: "",
     password: "",
-    role:"SDE"
+    role:""
   });
 
   const handleChange = (e) => {
@@ -112,6 +112,22 @@ export default function SignUp() {
               onChange={handleChange}
               required
               placeholder="Create a password"
+              className="w-full px-4 py-2 rounded-lg 
+                         bg-gray-100 dark:bg-slate-800 
+                         border border-gray-300 dark:border-slate-700 
+                         text-gray-900 dark:text-white
+                         focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div>
+            <input
+              type="text"
+              name="role"
+              value={formData.role}
+              onChange={handleChange}
+              required
+              placeholder="Enter your Role"
               className="w-full px-4 py-2 rounded-lg 
                          bg-gray-100 dark:bg-slate-800 
                          border border-gray-300 dark:border-slate-700 
