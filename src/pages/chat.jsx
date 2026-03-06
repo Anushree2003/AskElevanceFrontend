aa0d7c6abbb20a89d6bb2b539fa1bb698fe4301c
Anushree Sinha <anushree.sinha1@ibm.com>
2026-03-06 15:34:18 +0530

    Added loading feature

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ConversationSidebar from "../components/conversationsidebar";
 import ConversationPanel from "../components/conversationpanel";
 import { useTheme } from "../hooks/useTheme";
 import { useAuth } from "../context/authcontext";
 import { useParams, useNavigate } from "react-router-dom";
-import FaqPanel from "../components/faq";
+// import FaqPanel from "../components/faq";
 import ChatHome from "../components/chathome";
 
 export default function ChatPage() {
   const [isOpen, setIsOpen] = useState(true);
   const { isDark, toggleTheme } = useTheme();
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const { logout } = useAuth();
   const { sessionId } = useParams();
@@ -18,6 +19,26 @@ export default function ChatPage() {
     logout();
     navigate("/signin");
   };
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 700);  
+    return () => clearTimeout(timer);
+
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="h-screen flex items-center justify-center 
+                      bg-white dark:bg-slate-950">
+        <div className="w-10 h-10 border-4 border-blue-500 
+                        border-t-transparent rounded-full animate-spin">
+        </div>
+      </div>
+    );
+  }
 
   // when a message creates a new session, update route
   const handleNewMessage = (user, bot, newSessionId) => {
